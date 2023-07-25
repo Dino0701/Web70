f1ff4fc66b34984ca7ca5b10a3b8a995df41d0a4
Dino0701 <tuanpt701@gmail.com>
2023-07-26 02:07:43 +0700

    lesson 01 , nodejs overview

---
+++ BTWeb70/lesson-01-overview/index.js
@@ -0,0 +1,12 @@
+console.log("Hello MindX Web 70");
+
+// const { sum } = require("./math");
+// const sum = (a,b) => {
+//     return a + b;
+// };
+
+// const numOne = 4,
+//     numTwo = 5;
+//     const result = sum(numOne, numTwo);
+
+//     console.log({`Sum between ${numOne} + ${numTwo} = ${result}`});
